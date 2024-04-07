f8feb084051fd7c746afa0ff404e2ffe57b31f61
JGwi <gwizdzjakub2@gmail.com>
2024-04-07 16:15:53 +0200

    Creation of version 0.3

---
+++ labmaster_0.3/labmaster_03/src/main/java/com/labmaster/labmaster_03/Labmaster03Application.java
@@ -0,0 +1,13 @@
+package com.labmaster.labmaster_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labmaster03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labmaster03Application.class, args);
+	}
+
+}
