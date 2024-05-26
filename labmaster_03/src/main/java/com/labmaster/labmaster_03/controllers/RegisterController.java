5f1fd4e27dbaf5942aead26aade35d0cc694e7fc
justynajachowicz <jachowiczjustyna10@gmail.com>
2024-05-27 00:37:45 +0200

    Poprawki w stronach aplikacji, dodanie widoku rejestracji konta pracownika z poziomu administratora

---
+++ labmaster_03/src/main/java/com/labmaster/labmaster_03/controllers/RegisterController.java
@@ -0,0 +1,13 @@
+package com.labmaster.labmaster_03.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+    //@RequestMapping(value = "/login", method = RequestMethod.GET)
+    @GetMapping(value = "/register")
+    public String Register(){
+        return "register";
+    }
+}
